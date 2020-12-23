94fea8eab83855dc3ed8d4080357434846508416
Fernando dos Santos <ferdoferox@gmail.com>
2020-12-23 12:27:22 -0300

    Solução do primeiro teste.

---
+++ Teste_1/capitais_do_mundo.php
@@ -0,0 +1,13 @@
+<?php
+
+$location = ["Itália" => "Roma", "Romênia" => "Bucareste",
+            "Bélgica" => "Bruxelas", "Finlândia" => "Helsinki",
+            "França" => "Paris", "Alemanha" => "Berlim",
+            "Grécia" => "Atenas", "Irlanda" => "Dublin",
+            "Holanda" => "Amsterdã", "Espanha" => "Madri"];
+
+asort($location);
+
+foreach($location as $country => $capital) {
+    echo "A capital da $country é $capital"."\n" ;
+}
