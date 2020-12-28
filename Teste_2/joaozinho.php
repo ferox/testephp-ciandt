44bf5e5b56cb18486d942e257e73803b94c74397
Fernando dos Santos <ferdoferox@gmail.com>
2020-12-28 18:08:57 -0300

    Arrumando a lógica da segunda solução. Limpando e organizando o código.

--- Teste_2/joaozinho.php
+++ Teste_2/joaozinho.php
@@ -1,13 +1,8 @@
 <?php
 
-$porcentagem = 50;
+function foiMordido($number) { return ($number%2==0) ? 1 : 0; }
 
-function foiMordido($porcentagem)
-{
-    return mt_rand(1, 100) <= $porcentagem;
-}
-
-for ($i=0; $i < 20; $i++) {
-    echo (false !== foiMordido($porcentagem))
-    ? "Joãozinho mordeu o seu dedo !\n" : "Joãozinho NÃO mordeu o seu dedo !\n";
+$i = 0;
+for ($i;$i<10;$i++) {
+    echo ( foiMordido($i) ) ? "Joãozinho mordeu o seu dedo !\n" : "Joãozinho NÃO mordeu o seu dedo !\n";
 }
