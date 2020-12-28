cfd148f9cacb53f974a0e4bae103910870767100
Fernando dos Santos <ferdoferox@gmail.com>
2020-12-28 18:05:26 -0300

    Limpando e organizando o código da primeira soluçao.

--- Teste_1/capitais_do_mundo.php
+++ Teste_1/capitais_do_mundo.php
@@ -1,13 +1,21 @@
 <?php
 
-$location = ["Itália" => "Roma", "Romênia" => "Bucareste",
-            "Bélgica" => "Bruxelas", "Finlândia" => "Helsinki",
-            "França" => "Paris", "Alemanha" => "Berlim",
-            "Grécia" => "Atenas", "Irlanda" => "Dublin",
-            "Holanda" => "Amsterdã", "Espanha" => "Madri"];
+$location = [
+	"Itália" => "Roma",
+	"Romênia" => "Bucareste",
+	"Bélgica" => "Bruxelas",
+	"Finlândia" => "Helsinki",
+	"França" => "Paris",
+	"Alemanha" => "Berlim",
+	"Grécia" => "Atenas",
+	"Irlanda" => "Dublin",
+	"Holanda" => "Amsterdã",
+	"Espanha" => "Madri"
+];
 
 asort($location);
 
-foreach($location as $country => $capital) {
-    echo "A capital da $country é $capital"."\n" ;
+foreach($location as $country => $capital)
+{
+    echo "A capital da " . $country . " é " . $capital . "\n";
 }
